a9c3b0180687351fbbfce3a52319dd80e35cc0d7
dirkmcpherson <jstaley@Jamess-MacBook-Pro.local>
2020-02-08 10:48:43 -0500

    Mostly working

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 # import pandas as pd
 import numpy as np
-import matplotlib
 import time
+import matplotlib
+import os
 matplotlib.use('tkagg')
 
 import matplotlib.pyplot as plt
 
 from IPython import embed
 
+DEBUG = False
 Dimensions = 2
+WindVariance = 1
+WindMean = 0
+GPSVariance = 0.1
 
-A = np.array([[1, 1], [0, 1]]) # Matrix that maps previous state vector to new state vector 
-B = np.array([[0, 0], [0, 1]]) # Matrix that maps control signal to state vector changes
+
+if (Dimensions == 1):
+    A = np.array([[1, 1], [0, 1]]) # Matrix that maps previous state vector to new state vector 
+    B = np.array([[0, 0], [0, 1]]) # Matrix that maps control signal to state vector changes
+
+elif (Dimensions == 2):
+    A = np.array([[1,0,1,0],
+                 [0,1,0,1],
+                 [0,0,1,0],
+                 [0,0,0,1]])
+    B = np.array([[0,0,0,0],
+                 [0,0,0,0],
+                 [0,0,1,0],
+                 [0,0,0,1]])
+    
 
 
 # This is a vector of how the random variable (acceleration) effects each of the state variables
@@ -30,23 +48,36 @@ R = np.matmul(vertical, horizontal) # The covariance matrix of the noise (wind).
 print("R: {}".format(R))
 # timestep is 1, so its left out of all equations
 
+# Wrapper for np.transpose that also converts a row vector to a column vector and vica versa
+def customT(A):
+    if len(A.shape) == 1:
+        return A[:,None]
+    else:
+        return np.transpose(A)    
+
+#utility function for A * B * A_transpose
+def ABA_T(A, B):
+    return np.matmul(np.matmul(A, B), customT(A))
+
 class GroundTruth:
     def __init__(self):
-        self.position = np.zeros(Dimensions)
-        self.velocity = np.zeros(Dimensions)
+        self.state = np.zeros(2*Dimensions)
         self.positionHistory = []
         self.velocityHistory = []
         self.noiseHistory = []
 
-        self.noiseMean = 0.
-        self.noiseDeviation = 1.
-
     def GenerateNoise(self):
-        return np.array(np.random.normal(loc=self.noiseMean, scale=self.noiseDeviation, size=(Dimensions)))
+        return np.array(np.random.normal(loc=WindMean, scale=np.sqrt(WindVariance), size=(Dimensions)))
+
+    def position(self):
+        return self.state[0] if Dimensions == 1 else self.state[0:2]
+
+    def velocity(self):
+        return self.state[1] if Dimensions == 1 else self.state[2:]
 
     def update(self):
-        previousPosition = self.position
-        previousVelocity = self.velocity
+        previousPosition = self.position()
+        previousVelocity = self.velocity()
 
         self.positionHistory.append(previousPosition)
         self.velocityHistory.append(previousVelocity)
@@ -56,15 +87,18 @@ class GroundTruth:
         # self.velocity = previousVelocity + acceleration
         # self.position = previousPosition + self.velocity
 
-        x_t_1 = np.array([previousPosition, previousVelocity + 0.5 * acceleration])
+        previousVelocity += 0.5 * acceleration
+
+        if (Dimensions == 1):
+            x_t_1 = np.array([previousPosition, previousVelocity])
+        else:
+            x_t_1 = np.array([previousPosition[0], previousPosition[1], previousVelocity[0], previousVelocity[1]])
+        # x_t_1 = np.array([previousPosition, previousVelocity + 0.5 * acceleration])
         x_t = np.matmul(A,x_t_1)
-        # embed()
-        # time.sleep(1)
 
-        self.position = x_t[0]
-        self.velocity = x_t[1]
+        self.state = x_t
 
-        # print(x_t)
+        print(x_t)
         # print("{:2.2f}, {:2.2f}, {:2.2f}".format(self.position[0], self.velocity[0], acceleration[0]))
 
         self.noiseHistory.append(acceleration)
@@ -72,54 +106,83 @@ class GroundTruth:
 
 class KalmanFilter:
     def __init__(self):
-        self.position = np.zeros(Dimensions)
-        self.velocity = np.zeros(Dimensions)
+        self.state = np.zeros(2*Dimensions)
         self.positionHistory = []
         self.velocityHistory = []
-        self.ut = np.array([np.zeros(Dimensions), np.zeros(Dimensions)]) # control command (none)
+        self.ut = np.zeros(2*Dimensions) # control command (none)
         self.noiseMean = np.zeros(2 * Dimensions) # position and velocity for n-dimensions
+        self.covariance = np.identity(2*Dimensions) # nxn where n is the number of state variables
+
+    def position(self):
+        return self.state[0] if Dimensions == 1 else self.state[0:2]
+
+    def velocity(self):
+        return self.state[1] if Dimensions == 1 else self.state[2:]
 
     def update(self, z):
-        x_t_1 = np.array([self.position, self.velocity])
+        # from measurement
+        z_t = z.measurementHistory[-1]
+        C = z.C
+        Q = z.Q
+
+        x_t_1 = self.state
 
         stateUpdate = np.matmul(A,x_t_1)
         controlUpdate = np.matmul(B, self.ut)
-        epsilon = np.random.multivariate_normal(self.noiseMean, R) #TODO: what is R for dim > 1
+        # epsilon = np.random.multivariate_normal(self.noiseMean, R) #TODO: what is R for dim > 1
         # transpose epsilon so shape makes sense for all dimensions
         # rows = 1 if (len(epsilon.shape) == 1) else epsilon.shape[1] # edge case for 1d arrays 
         # cols = epsilon.shape[0]
-        epsilon = epsilon.reshape(2, Dimensions)
+        # epsilon = epsilon.reshape(2, Dimensions)
+
+        mean_bar = stateUpdate + controlUpdate
+        covariance_bar = ABA_T(A, self.covariance) + R# epsilon # Maybe this should be a square matrix
+
+        CEpC_T_Q = 1/(ABA_T(C, covariance_bar) + Q) if Dimensions == 1 else np.linalg.inv(ABA_T(C, covariance_bar) + Q)
+        K = np.matmul(np.matmul(covariance_bar, customT(C)), CEpC_T_Q) # Kalman gain
+        if (Dimensions == 1):
+            K = K[:,None]
+        print("Kalman Gain: ", K)
 
 
-        x_t = stateUpdate + controlUpdate + epsilon
-        embed()
-        time.sleep(1)
-        self.position = x_t[0]
-        self.velocity = x_t[1]
-        self.positionHistory.append(self.position)
-        self.velocityHistory.append(self.velocity)
-       
-        # embed()
-        # time.sleep(1)
+        mean = mean_bar + np.matmul(K, (z_t - np.matmul(C, mean_bar))) 
+        KC = np.multiply(K,C) if Dimensions == 1 else np.matmul(K,C)
+        I = np.identity(2*Dimensions)
+        covariance = np.matmul((I - KC), covariance_bar)
 
-        # print(x_t)
-        
-        # belief = np.linalg.det(np.pi*2*R)
+        if (DEBUG):
+            embed()
+            time.sleep(1)
 
+        self.covariance = covariance
+        self.state = mean
+        # x_t = stateUpdate + controlUpdate + epsilon
+
+        print(mean)
+
+        self.state = mean
+        self.positionHistory.append(self.position())
+        self.velocityHistory.append(self.velocity())
+    
 class GPS:
     def __init__(self):
-        self.variance = 1
-        self.C = np.array([1, 0])
-        self.Q = np.array([[self.variance, 0], [0, self.variance]])
+        if (Dimensions == 1):
+            self.C = np.array([1, 0])
+        else:
+            self.C = np.array([[1, 0, 0, 0],
+                              [0, 1, 0, 0]])
+        self.Q = np.array([GPSVariance]) if Dimensions == 1 else np.array([[GPSVariance, 0], [0, GPSVariance]]) # for 2d
         self.measurementHistory = []
         self.noiseMean = np.zeros(Dimensions) # position and velocity for n-dimensions
 
     def measure(self, groundTruth):
-        p = np.array([groundTruth.position, groundTruth.velocity]) # Don't need velocity but it keeps the dimensions in line with the books notation
+        p = groundTruth.state # Don't need velocity but it keeps the dimensions in line with the books notation
         
+        # embed()
+        # time.sleep(1)
         Cp = np.matmul(self.C, p)
     
-        delta = np.random.normal(self.noiseMean, self.variance) if Dimensions == 1 else np.random.multivariate_normal(self.noiseMean, self.Q) 
+        delta = np.random.normal(self.noiseMean, np.sqrt(GPSVariance)) if Dimensions == 1 else np.random.multivariate_normal(self.noiseMean, self.Q) 
         z = Cp + delta 
         self.measurementHistory.append(z)
 
@@ -129,26 +192,42 @@ def main():
     kf = KalmanFilter()
     z = GPS()
 
-    for i in range(10):
+    for i in range(100):
         gt.update()
         z.measure(gt)
-        kf.update(z.measurementHistory[-1])
+        kf.update(z)
 
-        print("diff: ", abs(z.measurementHistory[-1]-gt.positionHistory[-1]))
+        # print("diff: ", abs(z.measurementHistory[-1]-gt.positionHistory[-1]))
 
-    x = [entry[0] for entry in gt.positionHistory]
-    y = [entry[1] for entry in gt.positionHistory]
-    embed()
-    x1 = [entry[0] for entry in kf.positionHistory] 
-    y1 = [entry[1] for entry in kf.positionHistory]
+
+    if (Dimensions == 2):
+        x = [entry[0] for entry in gt.positionHistory]
+        x1 = [entry[0] for entry in kf.positionHistory] 
+        y = [entry[1] for entry in gt.positionHistory]
+        y1 = [entry[1] for entry in kf.positionHistory]
+        x2 = [entry[0] for entry in z.measurementHistory]
+        y2 = [entry[1] for entry in z.measurementHistory]
+        plt.plot(x, y, x1, y1, '--', x2, y2, 'k+')
+        plt.ylabel("Y position")
+        plt.xlabel("X position")
+
+    else:
+        x = [entry for entry in gt.positionHistory]
+        x1 = [entry for entry in kf.positionHistory] 
+        x2 = [entry for entry in z.measurementHistory]
+        t = [i for i in range(len(x))]
+        plt.plot(t, x, t, x1, '--', t, x2, 'k+')
+    # embed()
     # y = z.measurementHistory
 
 
-    t = [i for i in range(len(x))]
     # y = [entry[1] for entry in self.positionHistory] if (Dimensions > 1) else [i for i in range(len(x))]
-    plt.plot(x, y, x1, y1, '--')
-    plt.legend("Ground Truth", "Kalman Filter")
-    plt.show()
+    plt.legend(["Ground Truth", "Kalman Filter", "Measurements"])
+    plt.savefig('results.png', bbox_inches='tight')
+    from PIL import Image
+    with Image.open('results.png') as img:
+        img.show()
+    # plt.show() # this crashes everything on OSX
 
 
     # x0 = gt.positionHistory
@@ -174,4 +253,7 @@ def main():
     # plt.show()
 
 if __name__ == "__main__":
+    import sys
+    if (len(sys.argv) > 1):
+        DEBUG = sys.argv[1]
     main()
